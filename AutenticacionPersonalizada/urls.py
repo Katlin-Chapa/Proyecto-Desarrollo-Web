430f680167a323c694ed21b1905efce99215511f
JulioPonce77 <jmunozp7@miumg.edu.gt>
2024-10-03 20:43:16 -0600

    Implementacion de css al Inicio

--- AutenticacionPersonalizada/urls.py
+++ AutenticacionPersonalizada/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import InicioPersonalizado, UsuarioInicioVista
 from django.contrib.auth.views import LogoutView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('login/', UsuarioInicioVista.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('api/login/', InicioPersonalizado.as_view(), name='custom_login'),
+    
+  
+    path('', RedirectView.as_view(url='/login/')),
 ]
