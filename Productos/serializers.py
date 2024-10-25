4afbc13cc4fac3092bed98abaed5778e8d02d4e5
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-10-25 14:26:34 -0600

    borrado

--- Productos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Categoria, Producto
-
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = ["id", "nombre", "get_absolute_url", "descripcion", "precio", "get_imagen", "miniatura"]
--- Productos/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views  
-
-urlpatterns = [
-    path('ultimos-productos/', views.LatestProductsList.as_view(), name='ultimos-productos'),
-]
