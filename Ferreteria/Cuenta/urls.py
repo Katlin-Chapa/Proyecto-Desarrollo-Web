da5a25403565c2b11845ec0ecb8735094ba048e0
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-10-25 01:49:40 -0600

    cuenta modificar 2

--- Ferreteria/Cuenta/urls.py
+++ Ferreteria/Cuenta/urls.py
@@ -4,8 +4,6 @@ from .views import (
     LoginView,
     LogoutView,
     MisPedidosView,
-    EditarPerfilView,
-    CambiarContrasenaView
 )
 
 urlpatterns = [
@@ -13,6 +11,4 @@ urlpatterns = [
     path('iniciar-sesion/', LoginView.as_view(), name='iniciar_sesion'),
     path('cerrar-sesion/', LogoutView.as_view(), name='cerrar_sesion'),
     path('mis-pedidos/', MisPedidosView.as_view(), name='mis_pedidos'),
-    path('editar-perfil/', EditarPerfilView.as_view(), name='editar_perfil'),
-    path('cambiar-contrasena/', CambiarContrasenaView.as_view(), name='cambiar_contrasena'),
 ]
