c3a176f84774db42b4a12b8e35219912820d6028
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-10-27 15:15:09 -0600

    Regrese a esta copia de cuando funcionaba antes de las modificaciones extra

--- Ferreteria/Ferreteria/settings.py
+++ Ferreteria/Ferreteria/settings.py
@@ -90,14 +90,11 @@ WSGI_APPLICATION = 'Ferreteria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ferreteria',  
-        'USER': 'postgres',                   
-        'PASSWORD': '123',            
-        'HOST': 'localhost',                     
-        'PORT': '5432',                         
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
